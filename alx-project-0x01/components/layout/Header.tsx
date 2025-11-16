3e7f581b339ed6a8b1cc643dc2f04ccd49a906e1
Talent-1 <itzmhiztatalent@gmail.com>
2025-11-16 21:46:03 +0100

    Set up navigation between pages

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import Link from 'next/link';
+import Link from 'next/link'; 
 
 const Header: React.FC = () => {
-    return(
-        <header className = " h-20 bg-white shadow-md sticky p-4 top-0 z-10">
-            <div className =" max-w-7xl mx-auto gap-20 flex justify-center items-center">
-                <h1 className =" text-2xl font-bold text-indigo-700">
-                    ALX Blog
-                    </h1>
-                    <nav className='space-x-4 text-gray-600 font-medium'>
-                        <Link href="/" className = " hover:text-indigo-600">Home</Link>
-                        <Link href="/users" className = " hover:text-indigo-600">Users</Link>
-                        <Link href="/posts" className = " hover:text-indigo-600">Posts</Link>
-                    </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+     <Link href="/">Daily Contents</Link>
+  </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
